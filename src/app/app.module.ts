309cf0ae7167d5b85898e55aa224e323c783604a
christophe jeunesse <christophe.jeunesse@orange.com>
2021-01-14 21:31:34 +0100

    Ajout du routing pour test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {Routes, RouterModule} from '@angular/router';
+import { TestRouteComponent } from './test.route.component';
+
+
+const routes: Routes = [
+  {
+      path: 'sub',
+      component: TestRouteComponent
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent,TestRouteComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/test.route.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-route',
+  templateUrl: './test.route.component.html'
+})
+export class TestRouteComponent {
+}
